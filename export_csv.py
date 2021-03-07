e4293fefa524d7bf2ec7a9c443eab26366fd3740
Georgios Balikas <gbalikas@salesforce.com>
2021-03-07 22:25:30 +0100

    today data

---
+++ export_csv.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+page = requests.get("https://emvolio.gov.gr/vaccinationtracker")
+soup = BeautifulSoup(page.content, 'html.parser')
+df = soup.find_all('table', class_='table-striped table table-hover')
+df = pd.read_html(df[0].prettify())
+df = df[0]
+
+
+import datetime
+dt = datetime.datetime.today()
+fname = "data/{:04d}_{:02d}_{:02d}_{:02d}_{:02d}.csv".format(dt.year, dt.month, dt.day, dt.hour, dt.minute)
+print(fname)
+df.to_csv(fname, index=False, header=True)
