06f2404d54e0a9ef0ffbca10373bf7dc213bb3a0
Georgios Balikas <gbalikas@salesforce.com>
2021-03-26 15:05:13 +0100

    debug

--- export_csv.py
+++ export_csv.py
@@ -8,7 +8,9 @@ logging.basicConfig(level=logging.INFO)
 
 logging.info('starting')
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36', 
-"Referer": "https://emvolio.gov.gr/vaccinationtracker"}
+"Referer": "https://emvolio.gov.gr/vaccinationtracker",
+'sec-ch-ua': '"Chromium";v="89", "\"Not\\A;Brand";v="99", "Google Chrome";v="89"',
+    'sec-ch-ua-mobile': '?0',}
 page = requests.get("https://emvolio.gov.gr/vaccinationtracker", headers=headers)
 logging.info(page.status_code)
 soup = BeautifulSoup(page.content, 'html.parser')
